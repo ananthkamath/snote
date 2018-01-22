0a6236fef767c369d308f30bffb9b3ee84931af9
Ananth Kamath <ananth.atg@gmail.com>
2018-01-23 02:44:56 +0530

    User's were able to see others notes issue resolved

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :notes
   get 'welcome/index'
   devise_for :users
-  root to: 'welcome#index'
+  root to: 'notes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
